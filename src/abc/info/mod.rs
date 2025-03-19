e9eb1686cecade0cffb11f6c3ce00a89e0d51ffc
Athes <athesdrake@gmail.com>
2025-03-19 22:52:34 +0100

    [IMP] abc: make sub modules public

--- src/abc/info/mod.rs
+++ src/abc/info/mod.rs
@@ -1,12 +1,12 @@
-mod class;
-mod constant_pool;
-mod exception;
-mod metadata;
-mod method;
-mod multiname;
-mod namespace;
-mod script;
-mod r#trait;
+pub mod class;
+pub mod constant_pool;
+pub mod exception;
+pub mod metadata;
+pub mod method;
+pub mod multiname;
+pub mod namespace;
+pub mod script;
+pub mod r#trait;
 
 pub use class::Class;
 pub use constant_pool::ConstantPool;
@@ -15,5 +15,5 @@ pub use metadata::Metadata;
 pub use method::{Method, MethodFlag};
 pub use multiname::Multiname;
 pub use namespace::Namespace;
-pub use r#trait::{Trait, TraitAttr};
+pub use r#trait::{ITrait, Trait, TraitAttr};
 pub use script::Script;
